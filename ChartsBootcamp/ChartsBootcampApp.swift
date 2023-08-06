0963ebee2c1b2a6fadd2bb16df310e4f5c9e3b65
Artem <moiseev@physics.msu.ru>
2023-08-06 22:22:11 +0300

    Initial Commit

---
+++ ChartsBootcamp/ChartsBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsBootcampApp.swift
+//  ChartsBootcamp
+//
+//  Created by Artem on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChartsBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
